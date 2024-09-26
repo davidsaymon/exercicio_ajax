05cba4ac1f03ca71ac0f837c72916c15f91d248f
davidsaymon <davidh_saymon@hotmail.com>
2024-09-26 00:06:13 -0300

    Primeiro commit

---
+++ src/scripts/main.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const nameElement = document.querySelector('#name');
+    const usernameElement = document.querySelector('#username');
+    const avatarElement = document.querySelector('#avatar');
+    const reposElement = document.querySelector('#repos');
+    const followersElement = document.querySelector('#followers');
+    const followingElement = document.querySelector('#following');
+    const linkElement = document.querySelector('#link');
+
+    const endpoint = 'https://api.github.com/users/davidsaymon';
+
+    fetch(endpoint)
+        .then(function(res) {
+            if (!res.ok) {
+                throw new Error('Erro ao buscar os dados do GitHub');
+            }
+            return res.json();
+        })
+        .then(function(json) {
+            nameElement.innerText = json.name;
+            usernameElement.innerText = json.login;
+            avatarElement.src = json.avatar_url;
+            reposElement.innerText = json.public_repos;
+            followersElement.innerText = json.followers;
+            followingElement.innerText = json.following;
+            linkElement.href = json.html_url;
+        })
+        .catch(function(error) {
+            console.error('Erro ao buscar os dados:', error);
+        });
+});
